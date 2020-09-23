a9668475b759dc8c89fb608f4f2b0cb54e826751
박은식 <qkrdmstlr3@naver.com>
2020-09-23 21:27:46 +0900

    <REFACTOR>: shellHTML을 shellact로 변경
    - shellHTML을 shellact로 변경하였다
    	shellact는 customelement를 이용해서 자체적으로 만든 렌더링과 내부 상태관리를 위한 모듈이다

---
+++ src/lib/shellact.js
@@ -0,0 +1,31 @@
+function renderOutside([html, css], dom) {
+  dom.innerHTML = html;
+
+  if (css) {
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(css));
+    dom.createElement(style);
+  }
+}
+
+class Shellact extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.rerender();
+    this.state = {};
+  }
+
+  setState(state) {
+    this.state = state;
+    this.rerender();
+  }
+
+  render() {}
+
+  rerender() {
+    renderOutside(this.render(), this.shadowRoot);
+  }
+}
+
+export default Shellact;
