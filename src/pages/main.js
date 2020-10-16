c7c40feba47167ab593fbf5778358276b643f079
박은식 <qkrdmstlr3@naver.com>
2020-10-16 23:54:57 +0900

    <FEAT>: main페이지에 컴포넌트들을 불러옴
    - src/pages/main.js
    	main페이지에 필요한 컴포넌트들을 불러옴
    
    - src/style/calendarView.scss
    	스타일에 최대 넓이를 추가

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,9 +1,21 @@
 // lib
 import Shellact from 'lib/shellact';
 
+// components
+import 'components/calendarView';
+import 'components/register';
+import 'components/plan';
+
 class Main extends Shellact {
   render() {
-    return [`<div></div>`, null];
+    return [
+      `
+      <register-form></register-form>
+      <calendar-view></calendar-view>
+      <plan-list></plan-list>
+      `,
+      null,
+    ];
   }
 }
 
