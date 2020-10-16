61b9bdf83740b54b8ce8ac10f74d9fee5f1656e6
박은식 <qkrdmstlr3@naver.com>
2020-10-16 23:50:55 +0900

    <FEAT>: banner page 스타일링과 link태그 추가
    - src/pages/banner.js
    	html / css스타일링과 main페이지도 이동할 수 있는 link태그 추가
    
    - src/style/pages/banner.scss
    	banner page style

--- src/pages/banner.js
+++ src/pages/banner.js
@@ -1,9 +1,21 @@
 // lib
 import Shellact from 'lib/shellact';
+import 'lib/shell-router/link';
+
+// style
+import stylesheet from 'style/pages/banner.scss';
 
 class Banner extends Shellact {
   render() {
-    return [`<div></div>`, null];
+    return [
+      `
+      <div>
+        <h1>BANNER</h1>
+        <router-link to="/calendar">메인으로 이동</router-link>
+      </div>
+    `,
+      stylesheet,
+    ];
   }
 }
 
